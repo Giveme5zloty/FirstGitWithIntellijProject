2832409f0620222ba5da65f723d90794a37af4ec
Marek <marek_777_j@wp.pl>
2018-04-09 15:22:28 +0200

    Added super class Animal

---
+++ src/com/projects/strategyPattern/Animal.java
@@ -0,0 +1,23 @@
+package com.projects.strategyPattern;
+
+
+public abstract class Animal {
+    private String name;
+    private double speed;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getSpeed() {
+        return speed;
+    }
+
+    public void setSpeed(double speed) {
+        this.speed = speed;
+    }
+}
